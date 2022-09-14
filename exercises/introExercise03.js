2aca15fafc481e6f236ee22f5247cd2dc7013d56
Felipe Vidal <felipeisra.vidal@gmail.com>
2022-09-13 23:33:04 -0300

    add files

---
+++ exercises/introExercise03.js
@@ -0,0 +1,57 @@
+import * as THREE from  'three';
+import { OrbitControls } from '../build/jsm/controls/OrbitControls.js';
+import {initRenderer, 
+        initCamera,
+        initDefaultBasicLight,
+        setDefaultMaterial,
+        InfoBox,
+        onWindowResize,
+        createGroundPlaneXZ} from "../libs/util/util.js";
+
+let scene, renderer, camera, material, material2, material3, light, orbit;; // Initial variables
+scene = new THREE.Scene();    // Create main scene
+renderer = initRenderer();    // Init a basic renderer
+camera = initCamera(new THREE.Vector3(0, 15, 30)); // Init camera in this position
+material = setDefaultMaterial(); // create a basic material
+light = initDefaultBasicLight(scene); // Create a basic light to illuminate the scene
+orbit = new OrbitControls( camera, renderer.domElement ); // Enable mouse rotation, pan, zoom etc.
+
+// Listen window size changes
+window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+
+// Show axes (parameter is size of each axis)
+let axesHelper = new THREE.AxesHelper( 12 );
+scene.add( axesHelper );
+
+// create the ground plane
+let planeWidth = 20;
+let planeHeight = 20;
+let plane = createGroundPlaneXZ(planeWidth, planeHeight)
+scene.add(plane);
+
+for(var i = 0; i < 3; i++) {
+  for(var j = 0; j < 3; j++) {
+    let cubeSize = 4
+    let cubeGeometry = new THREE.BoxGeometry(cubeSize, cubeSize, cubeSize);
+    let cube = new THREE.Mesh(cubeGeometry, material);
+    cube.position.set(-8 + (i * 8), 2.0, -8 + (j * 8));
+    scene.add(cube);
+  }
+}
+
+// Use this to show information onscreen
+let controls = new InfoBox();
+  controls.add("Basic Scene");
+  controls.addParagraph();
+  controls.add("Use mouse to interact:");
+  controls.add("* Left button to rotate");
+  controls.add("* Right button to translate (pan)");
+  controls.add("* Scroll to zoom in/out.");
+  controls.show();
+
+render();
+function render()
+{
+  requestAnimationFrame(render);
+  renderer.render(scene, camera) // Render scene
+}
